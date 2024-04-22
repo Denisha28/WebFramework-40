5015765eb36e10a2ab593b86a0827b3d25752111
Denisha28 <124783467+Denisha28@users.noreply.github.com>
2024-04-22 12:33:46 +0530

    Delete DeniDay-6/class_exercise directory

--- DeniDay-6/class_exercise/Denicw1/src/main/java/com/example/class_exercise_1/Denirepository/DeniChildrenRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.Denicw.Denirepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.class_exercise_1.model.Children;
-
-public interface DeniChildrenRepo extends JpaRepository<DeniChildren, Integer> {
-
-}
